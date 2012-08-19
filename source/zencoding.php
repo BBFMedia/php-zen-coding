09a260c909709b887f1047794f3c475814e4d04a
Adrian Jones <adrianj1998@hotmail.com>
2012-08-19 23:47:43 +0800

    Added simple commnets

--- source/zencoding.php
+++ source/zencoding.php
@@ -1,205 +1,219 @@
 <?php
 
-/*
+/**
 
-$html = zencode('div>ul>li>p["text"]+p["text two"]');
+  $html = zencode('div>ul>li>p["text"]+p["text two"]');
 
-$var = array('test' => array('hello', 'world'));
-$html = zencode('div>ul>li>p["{test[$]}"]*2', $var);
+  $var = array('test' => array('hello', 'world'));
+  $html = zencode('div>ul>li>p["{test[$]}"]*2', $var);
 
-$html = zencode('a#test.thisclass.otherclass[ref="{test[0]}"]["content text "]>ul>li#$[href="{test[$]}link"]*2', $var);
-
-
-
-function myfunc1($str)
-{
-    return ucfirst($str);
-}
-function myfunc2($index)
-{
-    return 'This '.$index;
-}
-$var = array('a_function1'=>'myfunc1','a_function2'=>'myfunc2');
-$html = zencode('div["{a_function1("test")}+div["{a_function2($)}"]*3',$var);
-
-$var = array('test' => array('hello', 'world'));
-
-$html = zencode('div["{=  return $test[0] . " - ". $test[1]; }"]',$var);
+  $html = zencode('a#test.thisclass.otherclass[ref="{test[0]}"]["content text "]>ul>li#$[href="{test[$]}link"]*2', $var);
 
 
 
-$html = zencode('div["{=  $x = $test[0]; $y = $test[1]; return $x . " - " . $y; }"]',$var);
-*/
+  function myfunc1($str)
+  {
+  return ucfirst($str);
+  }
+  function myfunc2($index)
+  {
+  return 'This '.$index;
+  }
+  $var = array('a_function1'=>'myfunc1','a_function2'=>'myfunc2');
+  $html = zencode('div["{a_function1("test")}+div["{a_function2($)}"]*3',$var);
+
+  $var = array('test' => array('hello', 'world'));
+
+  $html = zencode('div["{=  return $test[0] . " - ". $test[1]; }"]',$var);
 
 
+
+  $html = zencode('div["{=  $x = $test[0]; $y = $test[1]; return $x . " - " . $y; }"]',$var);
+ */
+
+/**
+ * 
+ */
 class zenCode {
 
-function _replace($matches) {
-    global $var_data_zen, $var_index_zen;
-    $match = $matches[1];
-    foreach ($var_data_zen as $key => $value)
-    {
-        $$key = $value;
+    /**
+     * 
+     */
+    function _replace($matches) {
+        global $var_data_zen, $var_index_zen;
+        $match = $matches[1];
+        foreach ($var_data_zen as $key => $value) {
+            $$key = $value;
+        }
+
+        $match = str_replace('$', $var_index_zen, $match);
+
+        eval("\$data = $" . $match . ";");
+
+        return $data;
     }
 
-    $match = str_replace('$', $var_index_zen, $match);
+    /**
+     *
+     * @global type $var_data_zen
+     * @global type $var_index_zen
+     * @param type $matches
+     * @return type 
+     */
+    function _replace_code($matches) {
+        global $var_data_zen, $var_index_zen;
+        $match = $matches[1];
 
-    eval("\$data = $" . $match . ";");
+        foreach ($var_data_zen as $key => $value) {
+            $$key = $value;
+        }
 
-    return $data;
-}
+        $index = $var_index_zen;
+        $data = eval($match);
 
-function _replace_code($matches) {
-    global $var_data_zen, $var_index_zen;
-    $match = $matches[1];
-
-    foreach ($var_data_zen as $key => $value)
-    {
-        $$key = $value;
+        return $data;
     }
 
-    $index = $var_index_zen;
-    $data = eval($match);
+    /**
+     *
+     * @global type $var_data_zen
+     * @global type $var_index_zen
+     * @param type $html
+     * @param type $index
+     * @param type $var
+     * @return type 
+     */
+    function _replace_var($html, $index, $var) {
+        global $var_data_zen, $var_index_zen;
 
-    return $data;
-}
+        $pattern = '/{([\w$\[\]"()]+?)}/i'; // Matches any template tag
+        $callback = "_replace";
+        $var_data_zen = $var;
+        $var_index_zen = $index;
 
+        $html = preg_replace_callback($pattern, array(&$this, $callback), $html);
 
-function _replace_var($html, $index, $var) {
-    global $var_data_zen, $var_index_zen;
+        $pattern = '/{=(.*)}/i'; // Matches any template tag
+        $callback = "_replace_code";
 
-    $pattern = '/{([\w$\[\]"()]+?)}/i'; // Matches any template tag
-    $callback =  "_replace";
-    $var_data_zen = $var;
-    $var_index_zen = $index;
+        $html = preg_replace_callback($pattern, array(&$this, $callback), $html);
 
-    $html = preg_replace_callback($pattern, array( &$this, $callback), $html);
+        $html = preg_replace('/\$/', $index, $html);
+        return $html;
+    }
 
-    $pattern = '/{=(.*)}/i'; // Matches any template tag
-    $callback = "_replace_code";
+    /**
+     *
+     * @param type $code
+     * @param type $index
+     * @param type $var
+     * @return type 
+     */
+    function _parse($code, $index, $var) {
 
-    $html = preg_replace_callback($pattern, array( &$this, $callback), $html);
+        $t = 'dom';
+        $c = $code[$index];
+        $dom = $c['data'];
+        $data = array();
+        $i = 0;
+        $v = '';
+        $content = '';
+        while ($i < strlen($dom)) {
+            if ($dom[$i] == '.') {
 
-    $html = preg_replace('/\$/', $index, $html);
-    return $html;
-}
+                $data[$t][] = $v;
+                $t = 'class';
+                $v = '';
+            } elseif ($dom[$i] == '#') {
+                $data[$t][] = $v;
+                $t = 'id';
 
-
-function _parse($code, $index, $var) {
-
-    $t = 'dom';
-    $c = $code[$index];
-    $dom = $c['data'];
-    $data = array();
-    $i = 0;
-    $v = '';
-    $content = '';
-    while ($i < strlen($dom))
-    {
-        if ($dom[$i] == '.') {
-
-            $data[$t][] = $v;
-            $t = 'class';
-            $v = '';
-        }
-        elseif ($dom[$i] == '#') {
-            $data[$t][] = $v;
-            $t = 'id';
-
-            $v = '';
-        }
-        elseif ($dom[$i] == '[') {
-            $data[$t][] = $v;
-            $t = 'params';
-            $v = '';
-        }
-        elseif ($dom[$i] == ']') {
-            $data[$t][] = $v;
-            $t = 'E';
-            $v = '';
-        }
-        elseif ($dom[$i] == '{') {
-            while ($dom[$i] <> '}')
-            {
-                $v = $v . $dom[$i++];
+                $v = '';
+            } elseif ($dom[$i] == '[') {
+                $data[$t][] = $v;
+                $t = 'params';
+                $v = '';
+            } elseif ($dom[$i] == ']') {
+                $data[$t][] = $v;
+                $t = 'E';
+                $v = '';
+            } elseif ($dom[$i] == '{') {
+                while ($dom[$i] <> '}') {
+                    $v = $v . $dom[$i++];
+                }
+                $v = $v . $dom[$i];
+            } elseif ($dom[$i] == '*') {
+                $data[$t][] = $v;
+                $t = 'times';
+                $v = '';
             }
-            $v = $v . $dom[$i];
-
+            else
+                $v = $v . $dom[$i];
+            $i++;
         }
-        elseif ($dom[$i] == '*') {
-            $data[$t][] = $v;
-            $t = 'times';
-            $v = '';
-        }
-        else
-            $v = $v . $dom[$i];
-        $i++;
-    }
 
-    $data[$t][] = $v;
-    $times = 1;
-    if (isset($data['times'][0]))
-        $times = $data['times'][0];
-    $rot = 0;
-    $html = '';
-    while ($rot < $times)
-    {
+        $data[$t][] = $v;
+        $times = 1;
+        if (isset($data['times'][0]))
+            $times = $data['times'][0];
+        $rot = 0;
+        $html = '';
+        while ($rot < $times) {
 
 
-        $subhtml = '<' . $data['dom'][0];
-        if (isset($data['id']))
-            $subhtml .= ' id="' . $data['id'][0] . '"';
-        if (isset($data['class'])) {
-            $d = '';
-            foreach ($data['class'] as $cl)
-            {
-                $d .= ' ' . $cl;
+            $subhtml = '<' . $data['dom'][0];
+            if (isset($data['id']))
+                $subhtml .= ' id="' . $data['id'][0] . '"';
+            if (isset($data['class'])) {
+                $d = '';
+                foreach ($data['class'] as $cl) {
+                    $d .= ' ' . $cl;
+                }
+                $subhtml .= ' class="' . trim($d) . '"';
             }
-            $subhtml .= ' class="' . trim($d) . '"';
-        }
-        if (isset($data['params'])) {
-            foreach ($data['params'] as $pr)
-            {
-                if (preg_match('/(.*)="(.*)"$/', $pr))
-                    $subhtml .= ' ' . trim($pr);
-                else
-                    $content = ' ' . trim($pr, '"');
+            if (isset($data['params'])) {
+                foreach ($data['params'] as $pr) {
+                    if (preg_match('/(.*)="(.*)"$/', $pr))
+                        $subhtml .= ' ' . trim($pr);
+                    else
+                        $content = ' ' . trim($pr, '"');
+                }
             }
 
+            $subhtml .= '>' . $content;
+            if ((isset($code[$index + 1]['type'])) and ($code[$index + 1]['type'] == '>'))
+                $subhtml .= $this->_parse($code, $index + 1, $var);
+            $subhtml .= '</' . $data['dom'][0] . '>';
+            $subhtml = $this->_replace_var($subhtml, $rot, $var);
+            $html .= $subhtml;
+            $rot++;
+        }
+        if ((isset($code[$index + 1]['type'])) and ($code[$index + 1]['type'] == '+'))
+            $html .= $this->_parse($code, $index + 1, $var);
+
+        return $html;
+    }
+
+    /**
+     * 
+     * @param type $code
+     * @param type $var
+     * @return type 
+     */
+    function render($code, $var = array()) {
+
+        $zen = new zenCode();
+
+        $code = explode('>', $code);
+        $domarray = array();
+        foreach ($code as $item) {
+            $sub = explode('+', $item);
+            foreach ($sub as $key => $s)
+                $domarray[] = array('type' => ($key ? '+' : '>'), 'data' => $s);
         }
 
-        $subhtml .= '>' . $content;
-        if ((isset($code[$index + 1]['type'])) and ($code[$index + 1]['type'] == '>'))
-            $subhtml .= $this->_parse($code, $index + 1, $var);
-        $subhtml .= '</' . $data['dom'][0] . '>';
-        $subhtml = $this->_replace_var($subhtml, $rot, $var);
-        $html .= $subhtml;
-        $rot++;
-    }
-    if ((isset($code[$index + 1]['type'])) and ($code[$index + 1]['type'] == '+'))
-        $html .= $this->_parse($code, $index + 1, $var);
-
-    return $html;
-}
-
-function render($code, $var = array())
-
-{
-    
-    $zen = new zenCode();
-    
-    $code = explode('>', $code);
-    $domarray = array();
-    foreach ($code as $item)
-    {
-        $sub = explode('+', $item);
-        foreach ($sub as $key => $s)
-            $domarray[] = array('type' => ($key ? '+' : '>'), 'data' => $s);
-
+        $html = $zen->_parse($domarray, 0, $var);
+        return trim($html);
     }
 
-    $html = $zen->_parse($domarray, 0, $var);
-    return trim($html);
-
-}
 }
